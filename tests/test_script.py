3602c9fa46e29c65e3d56cfae052339eda81f01b
Leon Chen <lchen3@gmail.com>
2015-10-05 01:49:02 -0400

    add test and travis integration

---
+++ tests/test_script.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+import os
+import jupyter
+import unittest
+from jupythemer import jupythemer
+from jupythemer.jupythemer import custom_css_filepath
+from collections import namedtuple
+
+class TestScript(unittest.TestCase):
+    def setUp(self):
+        self.backup = ''
+        with open(custom_css_filepath, 'r') as f:
+            self.backup = f.read()
+
+    def tearDown(self):
+        with open(custom_css_filepath, 'w') as f:
+            f.write(self.backup)
+
+    def test_color(self):
+        args = namedtuple('args', ('color', 'layout', 'typography'))
+        jupythemer.run(args('neo', None, None))
+
+
+if __name__ == '__main__':
+    print('Test script')
+    unittest.main()
